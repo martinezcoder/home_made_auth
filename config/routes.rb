4785b42aeafe0ebc83087fec575516633bea66e5
Fran J Martinez <martinezcoder@gmail.com>
2016-03-19 02:27:30 +0100

    secure password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "sign_up" => 'users#new', as: "sign_up"
+  root :to => "users#new"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
