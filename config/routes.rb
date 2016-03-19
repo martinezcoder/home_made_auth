f3dff3dfbbf6982de2e64801e3494eb5beec2c2d
Fran J Martinez <martinezcoder@gmail.com>
2016-03-19 02:45:14 +0100

    log in authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "log_in" => 'sessions#new', as: "log_in"
   get "sign_up" => 'users#new', as: "sign_up"
   root :to => "users#new"
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
