a4128b8c382b4099c70fb866fa8d9e99e14e88b5
Fran J Martinez <martinezcoder@gmail.com>
2016-03-19 03:22:39 +0100

    scaffold name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_home_made_auth_session'
+Rails.application.config.session_store :cookie_store, key: '_scaffolds_session'
